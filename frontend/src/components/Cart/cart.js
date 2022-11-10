7066598362dd457fe2cd1fcaaf99708ee7125e15
hoangnguyen106 <nguyenhoangnhom4@gmail.com>
2022-11-10 18:29:51 +0700

    user

---
+++ frontend/src/components/Cart/cart.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+// import HeaderTop from "../header/header.top";
+import HeaderMiddle from "../Header/header.middle";
+// import HeaderBottom from "../Header/header.bottom";
+import ContentCart from "./content.cart";
+import Footer from "../Footer/footer";
+class Cart extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <header id="header">
+        
+          <HeaderMiddle
+            islogin={this.props.islogin}
+            logout={() => this.props.logout()}
+            history={this.props.history}
+          />
+        </header>
+        <ContentCart
+          islogin={this.props.islogin}
+          cart={this.props.cart}
+          updateProductInCart={product =>
+            this.props.updateProductInCart(product)
+          }
+          deteleProductInCart={(id_product) => this.props.deteleProductInCart(id_product)}
+    
+       
+          payment={( address, phone, name,total) => 
+            this.props.payment( address, phone, name,total)}
+          ispay={this.props.ispay}
+        />
+        <footer id="footer">
+          <Footer/>
+        </footer>
+      </div>
+    );
+  }
+}
+export default Cart;
---
+++ frontend/src/components/Header/header.js
@@ -0,0 +1,70 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+
+  return (
+    <>
+      {/* // Header top */}
+      <div className="header-middle">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="logo pull-left">
+                <a href="/">
+                  <img src="/assets/images/home/logo1.gif" alt="" />
+                </a>
+              </div>
+            </div>
+            <div className="col-sm-8">
+              <div className="shop-menu pull-right">
+                <ul className="nav navbar-nav collapse navbar-collapse">
+                  <li className='dropdown'>
+                    <a className='Setting-item'>
+                      <i className="fa fa-user dropbtn"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <Link to={"/cart"}>
+                      <i className="fa fa-shopping-cart" />
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* // Header bottom */}
+      <div className="header-bottom">
+        <div className="container">
+          <div className="row header-bot">
+            <div className="col-sm-8">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+              </div>
+
+            </div>
+            <div className="col-sm-4">
+              <div className="search_box pull-right">
+                <input
+                  type="text"
+                  placeholder="Search"
+                  onChange={(e) => this.props.setSearchText(e.target.value)}
+                  onKeyUp={(e) => this.handeSearch(e.keyCode)} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div></>
+  )
+}
+
+export default Header
