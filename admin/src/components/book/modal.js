781a485094e0f1206c24d64c1aadf1fb4cecdd63
Hoàng <nguyenhoangnhom4@gmail.com>
2023-09-11 19:00:43 +0700

    update-admin-modal-11/09

--- admin/src/components/book/modal.js
+++ admin/src/components/book/modal.js
@@ -1,14 +1,14 @@
 import "./modal.css";
 import React from "react";
 
-function Modal({ handleClose, show, children }) {
+function Modal({ handleClose, show, children, title }) {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
   return (
     <div className={showHideClassName}>
       <section className="modal-main">
         <div className="modal-header">
-          <h4>THÊM SÁCH</h4>
+          <h4>{title}</h4>
         </div>
         <div class="modal-body">{children}</div>
         <div class="modal-footer">
